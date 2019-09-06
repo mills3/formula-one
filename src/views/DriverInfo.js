a284ceda157e63319efc7946ab6d3d3766f18887
mills <16059882+mills3@users.noreply.github.com>
2019-09-06 13:18:55 +0100

    Fade in background images

--- src/views/DriverInfo.js
+++ src/views/DriverInfo.js
@@ -1,48 +1,20 @@
-import React, { useEffect, useState } from 'react';
-
-//EXAMPLE RESPONSE 
-const data = {
-  MRData: {
-    xmlns: "",
-    series: "",
-    url: "",
-    DriverTable: {
-      driverId: "verstappen",
-      Drivers: {
-        0: {
-          permanentNumber: "33",
-          givenName: "Max",
-          familyName: "Verstappen",
-          dateOfBirth: "1997-09-30",
-          nationality: "Dutch" 
-        }
-      }
-    }
-  }
-}
+import React, { useState } from 'react';
 
 const DriverInfo = ({ match }) => {
-  //DONT NEED ANY OF THIS!!!!!!!!   I THINK....................
-  // const [info, setInfo] = useState({});
 
-  // useEffect(() => {
-  //   getDriverInfo();
-  // }, []);
+  const [imgLoaded, setImgLoaded] = useState(false);
 
-  // const getDriverInfo = async () => {
-  //   const response = await fetch(`https://ergast.com/api/f1/drivers/${match.params.id}.json`);
-  //   const data = await response.json();
-  //   console.log(data);
-  //   setInfo(data.MRData.DriverTable.Drivers[0]);
-  // }
-
-  const [info, setInfo] = useState(data.MRData.DriverTable.Drivers[0]);
+  const handleImageLoad = () => {
+    setImgLoaded(true);
+  }
+  
+  //Fade in image once it has loaded by adding 'fade-in' className
+  const showImage = imgLoaded ? 'fade-in' : '';
 
   return (  
     <div className="driverinfo view">
-      <img src={`https://res.cloudinary.com/dzll7mejv/image/upload/v1566914937/f1-drivers/${match.params.id}.jpg`} alt="" />
+      <img src={`https://res.cloudinary.com/dzll7mejv/image/upload/v1566914937/f1-drivers/${match.params.id}.jpg`} alt="" onLoad={handleImageLoad} className={`${showImage}`} />
       <div className="aboveBgImg">
-        {/* <h1 className="page-heading">Driver Info</h1> */}
         <div className="driver-name">
             <h2>{ match.params.firstName }</h2>
             <h2>{ match.params.name }</h2>
@@ -57,5 +29,6 @@ const DriverInfo = ({ match }) => {
     </div>
   );
 }
+
  
 export default DriverInfo;
