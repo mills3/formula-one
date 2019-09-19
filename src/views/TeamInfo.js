b175feec34b9f1db6745e3f3caf68e8e4b54f154
mills <16059882+mills3@users.noreply.github.com>
2019-09-19 19:37:43 +0100

    Changed layout for teaminfo

--- src/views/TeamInfo.js
+++ src/views/TeamInfo.js
@@ -15,12 +15,11 @@ const TeamInfo = ({ match }) => {
   return (  
     <div className="teaminfo view">
       <h1 className="page-heading appear">{ match.params.name.toUpperCase() }</h1>
-      <div className="team-stats">
-        <p className="appear delay1">Position: <span>{ match.params.position }</span></p>
-        <p className="appear delay2">Points: <span>{ match.params.points }</span></p>
-        <p className="appear delay3">Nationality: <span>{ match.params.nation }</span></p>
+      <div className="team-stats appear delay1">
+        <p>Position: <span>{ match.params.position }</span></p>
+        <p>Points: <span>{ match.params.points }</span></p>
+        <p>Nationality: <span>{ match.params.nation }</span></p>
       </div>
-      {/* <img src={require('../assets/cars/mercedes-front.png')} alt="" /> */}
       <img 
         src={`https://res.cloudinary.com/dzll7mejv/image/upload/v1568053287/f1-cars/${match.params.id}.png`} 
         alt="" 
