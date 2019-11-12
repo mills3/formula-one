9fbd5a59e8971feb1d22d2202a5616b254ff6d92
mills <16059882+mills3@users.noreply.github.com>
2019-11-12 20:24:23 +0000

    Many small changes, all amazing!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+// import './App.css';
+import './App2.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import LeaderBoards from './views/LeaderBoards';
--- src/views/DriverInfo.js
+++ src/views/DriverInfo.js
@@ -15,15 +15,15 @@ const DriverInfo = ({ match }) => {
     <div className="driverinfo view">
       <img src={`https://res.cloudinary.com/dzll7mejv/image/upload/v1566914937/f1-drivers/${match.params.id}.jpg`} alt="" onLoad={handleImageLoad} className={`${showImage}`} />
       <div className="aboveBgImg">
-        <div className="driver-name page-heading appear">
+        <div className="driver-name page-heading appear delay2">
             <h2>{ match.params.firstName }</h2>
             <h2>{ match.params.name }</h2>
           </div>
-          <h1 className="outlined-text driver-number appear">{ match.params.number }</h1>
-        <div className="stats appear delay1">  
-          <p>points: <span>{ match.params.points }</span></p>        
-          <p>position: <span>{ match.params.position }</span></p>          
-          <p>nationality: <span>{ match.params.nation }</span></p>
+          <h1 className="outlined-text driver-number appear delay1">{ match.params.number }</h1>
+        <div className="stats appear">  
+          <p>POINTS: <span>{ match.params.points }</span></p>        
+          <p>POSITION: <span>{ match.params.position }</span></p>          
+          <p>NATIONALITY: <span>{ match.params.nation }</span></p>
         </div>
       </div>      
     </div>
